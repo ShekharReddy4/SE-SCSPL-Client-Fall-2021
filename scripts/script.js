ce8bc04443e65bf7c0df0d1bb63ae8d7a5632537
Shekhar Reddy Mittapelly <shekharreddymittapelly@Shekhars-MacBook-Air.local>
2021-11-27 07:05:10 -0600

    added bootstrap etc.,

---
+++ scripts/script.js
@@ -0,0 +1,14 @@
+
+var xhr = new XMLHttpRequest();
+xhr.withCredentials = true;
+
+xhr.addEventListener("readystatechange", function() {
+  if(this.readyState === 4) {
+    console.log(this.responseText);
+  }
+});
+
+xhr.open("GET", "https://localhost:44343/weatherforecast");
+
+xhr.send();
+
